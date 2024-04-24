55cbd471b5c0fd811b225b31abb5d624da6fbf7b
Softmaks <maxviruk@gmail.com>
2024-04-24 18:33:35 +0300

    1
    1

--- js_hm_4.js
+++ js_hm_4.js
@@ -142,20 +142,20 @@
 // Example 8 - Форматирование времени
 // Напиши функцию formatTime(minutes) которая переведёт значение minutes (количество минут) в строку в формате часов и минут HH:MM.
 
-// const hours = Math.floor(totalMinutes / 60);
-// const minutes = totalMinutes % 60;
-// console.log(hours);
-// console.log(minutes);
+const hours = Math.floor(totalMinutes / 60);
+const minutes = totalMinutes % 60;
+console.log(hours);
+console.log(minutes);
 
-// const doubleDigitHours = String(hours).padStart(2, 0);
-// const doubleDigitMinutes = String(minutes).padStart(2, 0);
-// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
+const doubleDigitHours = String(hours).padStart(2, 0);
+const doubleDigitMinutes = String(minutes).padStart(2, 0);
+console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
 
-// function formatTime(minutes) {}
+function formatTime(minutes) {}
 
-// console.log(formatTime(70)); // "01:10"
-// console.log(formatTime(450)); // "07:30"
-// console.log(formatTime(1441)); // "24:01"
+console.log(formatTime(70)); // "01:10"
+console.log(formatTime(450)); // "07:30"
+console.log(formatTime(1441)); // "24:01"
 
 //  ---------------------------------------------------------------------------------------------------------------------------------
 
